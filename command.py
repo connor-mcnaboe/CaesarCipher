7bfb1b440aebc6c7f21649cc0b8922673dd8bd52
zerodarkbirdy <connor.mcnaboe@uconn.edu>
2017-03-11 22:37:24 -0500

    Update command.py

--- command.py
+++ command.py
@@ -1,7 +1,7 @@
 import argparse
 from def_cipher import encrypt, decrypt, rtnTxt, ascii, main
 parser = argparse.ArgumentParser() # Define comamand line args
-parser.add_argument("-k", type=int, help="Enter a key between 0 and 256")
+parser.add_argument("-k", type=int, help="Enter a keyvalue")
 parser.add_argument("-f", help="Enter filename that will be encrypted") 
 parser.add_argument("-e", help="Option to encrypt", action="store_true") 
 parser.add_argument("-d", help="Option to decrypt", action="store_true") 
